96062b4749d2658b7672708aba6905b33bf61884
dtg-lucifer <piushbasu@gmail.com>
2025-05-30 13:31:23 +0530

    code: made the whole site more responsive, need to add animations

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -29,16 +29,16 @@ import {
 const About = () => {
   return (
     <div className="flex flex-col justify-center items-center min-h-screen">
-      <h2 className="w-full font-black text-6xl sm:text-8xl text-right">
+      <h2 className="w-full font-black text-6xl sm:text-8xl">
         Who am I?
       </h2>
-      <h3 className="w-full font-secondary font-bold text-2xl text-right">
+      <h3 className="w-full font-secondary font-bold text-2xl">
         Cloud & DevOps Facilitator ~ Google DGoC TIU
       </h3>
-      <h3 className="w-full font-secondary font-bold text-2xl text-right">
+      <h3 className="w-full font-secondary font-bold text-2xl">
         Ex. Full Stack Developer ~ The Entrepreneurship Network
       </h3>
-      <p className="mt-4 w-full text-base sm:text-2xl text-right">
+      <p className="mt-4 w-full text-base sm:text-2xl">
         I am a software engineer with a passion for building applications that
         make a difference. I have experience in various technologies and love to
         learn new things.
@@ -46,16 +46,16 @@ const About = () => {
 
       <hr className="bg-dark my-4 mt-20 sm:mt-[10rem] w-full h-[1px]" />
 
-      <h2 className="w-full font-black text-2xl sm:text-4xl text-right">
+      <h2 className="w-full font-black text-2xl sm:text-4xl">
         Let&apos;s just be honest, I am a nerd, and I love to learn new things.
       </h2>
 
       <hr className="bg-dark my-4 mb-20 sm:mb-[10rem] w-full h-[1px]" />
 
-      <h2 className="w-full font-black text-4xl text-right">
+      <h2 className="w-full font-black text-4xl">
         Frameworks I love
       </h2>
-      <p className="flex flex-wrap justify-end gap-4 mt-4 mb-8 w-full text-base sm:text-2xl text-right">
+      <p className="flex flex-wrap gap-4 mt-4 mb-8 w-full text-base sm:text-2xl">
         <FaReact size={40} />
         <RiNextjsFill size={40} />
         <SiTailwindcss size={40} />
@@ -67,10 +67,10 @@ const About = () => {
         <SiActix size={40} />
       </p>
       <hr className="md:hidden bg-dark my-4 mt-20 sm:mt-[10rem] w-full h-[1px]" />
-      <h2 className="w-full font-black text-4xl text-right">
+      <h2 className="w-full font-black text-4xl">
         Languages I know
       </h2>
-      <p className="flex flex-wrap justify-end gap-4 mt-4 mb-8 w-full text-base sm:text-2xl text-right y-4">
+      <p className="flex flex-wrap gap-4 mt-4 mb-8 w-full text-base sm:text-2xl y-4">
         <SiTypescript size={35} />
         <FaGolang size={40} />
         <FaRust size={40} />
@@ -79,20 +79,20 @@ const About = () => {
         <SiCplusplus size={35} />
       </p>
       <hr className="md:hidden bg-dark my-4 mt-20 sm:mt-[10rem] w-full h-[1px]" />
-      <h2 className="w-full font-black text-4xl text-right">
+      <h2 className="w-full font-black text-4xl">
         Tools I use
       </h2>
-      <p className="flex flex-wrap justify-end gap-4 mt-4 mb-8 w-full text-base sm:text-2xl text-right">
+      <p className="flex flex-wrap gap-4 mt-4 mb-8 w-full text-base sm:text-2xl">
         <BiLogoVisualStudio size={40} />
         <SiIntellijidea size={35} />
         <SiPostman size={35} />
         <SiDocker size={35} />
       </p>
       <hr className="md:hidden bg-dark my-4 mt-20 sm:mt-[10rem] w-full h-[1px]" />
-      <h2 className="w-full font-black text-4xl text-right">
+      <h2 className="w-full font-black text-4xl">
         Cloud & DevOps stuffs that I use
       </h2>
-      <p className="flex flex-wrap justify-end gap-4 mt-4 mb-8 w-full text-base sm:text-2xl text-right">
+      <p className="flex flex-wrap gap-4 mt-4 mb-8 w-full text-base sm:text-2xl">
         <SiGooglecloud size={35} />
         <FaAws size={40} />
         <SiKubernetes size={35} />
--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -16,7 +16,7 @@ const Projects = () => {
   return (
     <div className="flex flex-col justify-center items-center mt-40 md:mt-0 w-full min-h-screen">
       <h2 className="mb-6 md:mb-10 lg:mb-20 w-full font-black text-5xl md:text-7xl">
-        Some great stuffs I built
+        Some of my best projects
       </h2>
       <aside className="gap-4 sm:gap-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 w-full">
         {projects.map((project) => (
